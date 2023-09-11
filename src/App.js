5c52d6bf24aa2d18d15c9d6180403be0082fc882
Rodrigo Lopez <scdtp2019@gmail.com>
2023-09-11 19:32:42 +0800

    Setup w3 zoom in effect & install router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './components/Header'
 import About from './components/About'
+import AboutPage from './components/AboutPage'
 import Portfolio  from './components/Portfolio'
 import Contact  from './components/Contact'
 import Footer  from './components/Footer'
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,23 +8,23 @@ const Portfolio = () => {
     <div className="portfolio">
         <h2 className="portfolio__heading--h2">MY PORTFOLIO</h2>
         <div className="portfolio__container ">
-            <div className="portfolio__img"  >
+            <div className="portfolio__img w3-animate-bottom w3-animate-zoom"  >
                 <a href="https://kg-2023.github.io/RL-TeaShop/" target="_blank"> 
                  <img src={tea} alt="Tea Shp" />
 
                
                 </a>
             </div>
-            <div className="portfolio__img"  >
+            <div className="portfolio__img w3-animate-bottom w3-animate-zoom"  >
                 <img loading="lazy"
                     src="https://github.com/KG-2023/Activity-Images-2023/blob/main/pexels-kat-smith-551628.jpg?raw=true"
                     alt="smiling dog" />
             </div>
-            <div className="portfolio__img"><a href="https://html-form-flax.vercel.app/" target="_blank"><img loading="lazy"
+            <div className="portfolio__img w3-animate-bottom"><a href="https://html-form-flax.vercel.app/" target="_blank"><img loading="lazy"
                 src="https://github.com/KG-2023/Activity-Images-2023/blob/main/pexels-fauxels-3184418.jpg?raw=true"
                 alt="team of people" /></a>
             </div>
-            <div className="portfolio__img">
+            <div className="portfolio__img w3-animate-bottom w3-animate-zoom">
                 <img loading="lazy"
                         src="https://github.com/KG-2023/Activity-Images-2023/blob/main/pexels-fauxels-3184418.jpg?raw=true"
                         alt="team of people" />
